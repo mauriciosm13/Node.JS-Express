59b7b6cc33de92806a9dc54b55f35d606b8bfeb0
mauriciosm13 <70268675+mauriciosm13@users.noreply.github.com>
2022-03-11 11:01:11 -0300

    Commit
    Teste com jest

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,20 @@
 const pegarArquivo = require('../index');
 
- test('deve ser uma function', () => {
-     expect(typeof pegarArquivo).toBe('function');
- });
+const arrayResultado = [{
+    FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'  
+}]
+
+describe('pegarArquivo::',() => {
+    it('Deve ser uma função', () =>{
+        expect(typeof pegarArquivo).toBe('function');
+    })
+    it('deve retornar array com resultados', async () => {
+        const resultado = await pegarArquivo('/Users/Engloba Sistemas/Documents/GitHub/Node.JS-Express/test/arquivos/texto1.md')
+        expect(resultado).toEqual(arrayResultado)
+    })
+    it('deve retornar mensagem "não há links"', async () => {
+        const resultado = await pegarArquivo('/Users/Engloba Sistemas/Documents/GitHub/Node.JS-Express/test/arquivos/texto1_semlinks.md')
+        expect(resultado).toBe('Não há links')
+    })
+})
+ 
